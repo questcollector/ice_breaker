517758ead2020bcf98ff3ec519305719ed76035e
kiyoung.you <miroirs01@gmail.com>
2023-10-19 14:42:35 +0900

    add pydantic output parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,6 +2,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
+from output_parsers import person_intel_parser, PersonIntel
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
@@ -17,30 +18,41 @@ from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 # Musk has expressed views that have made him a polarizing figure. He has been criticized for making unscientific and misleading statements, including spreading COVID-19 misinformation and promoting conspiracy theories. His Twitter ownership has been similarly controversial, including laying off a large number of employees, an increase in hate speech on the platform, and changes to Twitter Blue verification. In 2018, the U.S. Securities and Exchange Commission (SEC) sued him for falsely tweeting that he had secured funding for a private takeover of Tesla. To settle the case, Musk stepped down as the chairman of Tesla and paid a $20 million fine.
 # """
 
-name = "eden marco udemy"
 
-if __name__ == "__main__":
-    print("Hello LangChain")
-
-    linkedin_profile_url = linkedin_lookup_agent(name=name)
+def ice_break(name: str) -> PersonIntel:
+    # linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_profile_url = "https://www.linkedin.com/in/eden-marco-udemy"
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url)
 
-    twitter_username = twitter_lookup_agent(name=name)
+    # twitter_username = twitter_lookup_agent(name=name)
+    twitter_username = "@EdenEmarco177"
     tweets = scrape_user_tweets(username=twitter_username, num_tweets=100)
 
     summary_template = """
         given the information {linkedin_information} and twitter {twitter_information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
+        3. A topic that may interest them
+        4. 2 creative Ice breakers to open a conversation with them
+        \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    output = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    print(output)
+    return person_intel_parser.parse(output)
+
+
+if __name__ == "__main__":
+    print("Hello LangChain")
+    ice_break("eden marco udemy")
